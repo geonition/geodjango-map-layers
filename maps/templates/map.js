02a2d83610bbceda9f82fd559695ba33c059302b
Sakari Ellonen <sakari.ellonen@gmail.com>
2013-08-21 15:19:52 +0300

    got rid of some repetition

--- maps/templates/map.js
+++ maps/templates/map.js
@@ -41,6 +41,7 @@ callback_function -- callback function the to be called after creation
     The callback_function will get the map as a parameter
 */
 gnt.maps.create_map = function (map_div, callback_function) {
+    var layer;
     {% if map_data.google_key %}
     if(!gnt.maps.GoogleLoaded) {
         gnt.maps.map_div = map_div;
@@ -57,7 +58,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
 
         {% if layer.protocol == 'OSM-standard' %}
         layer = new OpenLayers.Layer.OSM('OSM-standard');
-        gnt.maps.layers.push(layer);
         {% endif %}
 
 
@@ -66,7 +66,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
                                         type: google.maps.MapTypeId.SATELLITE,
                                         numZoomLevels: 22
                                         });
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'Google-hybrid' %}
@@ -74,7 +73,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
                                         type: google.maps.MapTypeId.HYBRID,
                                         numZoomLevels: 22
                                         });
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'Google-road' %}
@@ -82,14 +80,12 @@ gnt.maps.create_map = function (map_div, callback_function) {
                                         type: google.maps.MapTypeId.ROAD,
                                         numZoomLevels: 22
                                         });
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'Google-terrain' %}
         layer = new OpenLayers.Layer.Google('Terrain', {
                                         type: google.maps.MapTypeId.TERRAIN
                                         });
-        gnt.maps.layers.push(layer);
         {% endif %}
         {% if layer.protocol == 'Bing-satellite' %}
         layer = new OpenLayers.Layer.Bing({
@@ -97,7 +93,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
                                         type: "AerialWithLabels",
                                         key: "AjB69asvfCy_FaIvDNBzCFc2eJdF7m7_bA7-M-xpJKctrxjmYQjqYX5DRCH0sd3J",
                                         culture: "en"});
-        gnt.maps.layers.push(layer);
         {% endif %}
         {% if layer.protocol == 'Imagefile' %}
         size = "{{layer.layer_info}}".split(',');
@@ -107,7 +102,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
                         new OpenLayers.Bounds(0,0,size[0],size[1]),
                         new OpenLayers.Size(size[0], size[1]),
                         {numZoomLevels: 3});
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'OSM-cyclemap' %}
@@ -115,7 +109,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
             ["http://a.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png",
             "http://b.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png",
             "http://c.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"]);
-        gnt.maps.layers.push(layer);
         {% endif%}
 
         {% if layer.protocol == 'OSM-mapquest' %}
@@ -124,7 +117,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
              "http://otile2.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
              "http://otile3.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
              "http://otile4.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg"]);
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'OSM-watercolor' %}
@@ -133,7 +125,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
              "http://b.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg",
              "http://c.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg",
              "http://d.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg"]);
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'ArcGISCache' %}
@@ -146,7 +137,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
                     attribution: layer_info.copyrightText
                     }
                 );
-        gnt.maps.layers.push(layer);
 
         mapOptions = {
             maxExtent: layer.maxExtent,
@@ -168,7 +158,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
             {{ layer.layer_info|safe }},
             {isBaseLayer: false}
         );
-        gnt.maps.layers.push(layer);
         {% endif %}
         if(map_options_created === false) {
             mapOptions = {
@@ -199,7 +188,6 @@ gnt.maps.create_map = function (map_div, callback_function) {
         mapOptions.matrixIds = matrixIds;
         mapOptions.attribution = '<a target="_blank" href="{{ layer.attr_url}}">{{ layer.attr_title|safe }}</a>'
         layer = new OpenLayers.Layer.WMTS(mapOptions);
-        gnt.maps.layers.push(layer);
         {% endif %}
 
         {% if layer.protocol == 'WMS' %}
@@ -214,8 +202,8 @@ gnt.maps.create_map = function (map_div, callback_function) {
                      attribution: '<a target="_blank" href="{{ layer.attr_url}}">{{ layer.attr_title|safe }}</a>'
                     });
 
-        gnt.maps.layers.push(layer);
         {% endif %}
+        gnt.maps.layers.push(layer);
     {% endfor %}
 
     //make sure mapOptions controls are set correct
